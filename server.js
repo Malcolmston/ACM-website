a4f274b5db800b6f11752e8475cba7630e0018bf
alcolmston <mstone@crefeld.org>
2024-04-09 12:10:57 -0400

    render files renders a file `home.html` to server to users

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
+var fs = require('fs');
 
-//create a server object:
+
 http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-  }).listen(8080); //the server object listens on port 8080
+  fs.readFile('home.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);

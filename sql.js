d788c6455a9324421d51aca344df82a32d93272e
malcolmstone <mstone@crefeld.org>
2024-04-22 12:25:24 -0400

    adds in a table and general sql testing adds a database table for events to be added to. this will allow people to create events to add to a table

--- sql.js
+++ sql.js
@@ -5,3 +5,40 @@ const sequelize = new Sequelize({
     storage: 'database.db'
   });
   
+
+  const Events = sequelize.define('events', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    file: {
+        type: DataTypes.BLOB
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    start: {
+        type: DataTypes.DATE,
+        defaultValue: new Date()
+    },
+    from: {type: DataTypes.DATE},
+    to: {type: DataTypes.DATE}
+  });
+  
+  (async function () {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+
+      await sequelize.sync({ force: true });
+
+  })();
